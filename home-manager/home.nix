fa93d1ddc6380ba7bcc41ce8a156198d6d076650
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-31 10:22:49 +0900

    feat(home-manager/git/.gitconfig): cp run ~/.gitconfig to home-manager/git/.gitconfig

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,7 +17,7 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs;[
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -34,8 +34,7 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
-    nixfmt
-        # shell
+    # shell
     starship
     zoxide
     fzf
