548b9e0ae71ba1cb283cadc59b555ada5b2170e6
w4daka <213398204+w4daka@users.noreply.github.com>
2026-09-01 10:40:04 +0900

    fix(home-manager/nvim/lua/languages/markdown.lua): 空のplugin specを削除
    旧markdown-preview.nvimを削除

--- home-manager/nvim/lua/languages/markdown.lua
+++ home-manager/nvim/lua/languages/markdown.lua
@@ -1,19 +1,5 @@
 -- install without yarn or npm
 return {
-  -- ~/.config/nvim/lua/plugins.lua または init.lua 内
-  {
-    -- "iamcco/markdown-preview.nvim",
-    -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    -- build = function()
-    --   local app_dir = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
-    --   vim.fn.system("cd " .. app_dir .. " && npm install")
-    -- end,
-    -- init = function()
-    --   vim.g.mkdp_filetypes = { "markdown" }
-    --   vim.g.mkdp_browser = "/usr/bin/vivaldi-stable" -- 明示指定で環境変数依存を排除
-    -- end,
-    -- ft = { "markdown" },
-  },
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
